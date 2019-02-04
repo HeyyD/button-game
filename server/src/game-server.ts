3886de6e34eba3648fc54ad442b8091dc59e3fac
HeyyD <heidi.hati@cs.tamk.fi>
2019-02-04 14:26:13 +0200

    add io socket

--- server/src/game-server.ts
+++ server/src/game-server.ts
@@ -1,6 +1,6 @@
 import { createServer, Server } from 'http';
 import * as express from 'express';
-// import * as socketIo from 'socket.io';
+import * as socketIo from 'socket.io';
 
 export class GameServer {
 
@@ -8,19 +8,30 @@ export class GameServer {
 
   private app: express.Application;
   private server: Server;
+  private io: SocketIO.Server;
   private port: string | number;
 
   constructor() {
     this.app = express();
     this.server = createServer(this.app);
+    this.io = socketIo(this.server)
+
     this.port = process.env.PORT || GameServer.PORT;
 
-    this.server.listen(this.port, () => {
-      console.log(`Running server on port ${this.port}`);
-    });
+    this.init();
   }
 
   getApp(): express.Application {
     return this.app;
+  }
+
+  private init(): void {
+    this.server.listen(this.port, () => {
+      console.log(`Running server on port ${this.port}`);
+    });
+
+    this.io.on('connect', (socket: any) => {
+      console.log(`Connected client on port ${this.port}`);
+    })
   }
 }
